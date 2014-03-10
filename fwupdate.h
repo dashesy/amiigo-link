1960a76b0a40fc385ebc4d09f80d0eddad123351
Ehsan Azarnasab <dashesy@gmail.com>
2014-03-10 14:09:03 -0600

    refactor fwupdate to its own unit

---
+++ fwupdate.h
@@ -0,0 +1,14 @@
+/*
+ * Amiigo Link firmware update process
+ *
+ * @date March 10, 2014
+ * @author: dashesy
+ */
+
+#ifndef FWUPDATE_H
+#define FWUPDATE_H
+
+int set_update_file(const char * szName);
+int process_fwstatus(amdev_t * dev, uint8_t * buf, ssize_t buflen);
+
+#endif // include guard
