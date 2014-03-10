301f0906340fe085e1cbaf6ba19e1141f94f79a7
Ehsan Azarnasab <dashesy@gmail.com>
2014-03-09 20:26:28 -0600

    refactor amiigo device

---
+++ amdev.h
@@ -0,0 +1,45 @@
+/*
+ * Amiigo device
+ *
+ * @date March 9, 2014
+ * @author: dashesy
+ */
+
+#ifndef AMDEV_H
+#define AMDEV_H
+
+#include "amidefs.h"
+
+typedef enum _DISCOVERY_STATE {
+    STATE_NONE = 0,
+    STATE_BUILD,
+    STATE_VERSION,
+    STATE_STATUS,
+    STATE_DOWNLOAD,
+    STATE_FWSTATUS,
+    STATE_FWSTATUS_WAIT,
+    STATE_I2C,
+
+    STATE_COUNT, // This must be the last
+} DISCOVERY_STATE;
+
+// Keep the state of each device here
+typedef struct _amdev {
+    DISCOVERY_STATE state;     // Device state machine state
+    int sock;                  // Socket openned for this device
+    WEDVersion ver;            // Firmware version
+    unsigned int ver_flat = 0; // Flat version number to compare
+    WEDStatus status;          // Firmware status
+    struct {
+        uint8 type; // WED_LOG_TAG
+        // Tag data from WED_MAINT_TAG command
+        uint32_t tag;
+    } PACKED logTag;           // Last tag
+    WEDLogTimestamp logTime;   // Last timestamp packet
+    WEDLogAccel logAccel;      // Last accel
+    uint32_t read_logs = 0;    // Logs downloaded so far
+    uint32_t total_logs = 0;   // Total number of logs tp be downloaded
+    int bValidAccel = 0;     // If any uncompressed accel is received
+} amdev_t;
+
+#endif // include guard
